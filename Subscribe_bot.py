15824ad712d88ea72f2ac08108f8e2a8f6551f64
AlphaTangoCharlie <112169487+AlphaTangoCharlie@users.noreply.github.com>
2022-08-26 20:23:00 +1000

    Update Subscribe_bot.py

--- Subscribe_bot.py
+++ Subscribe_bot.py
@@ -22,7 +22,7 @@ class SubBot:
     bellButtonClick = "Bell[1].click();"    
 
     # Type here your channel url:
-    url = input("Enter your channel link = ")
+    url = input("https://www.youtube.com/channel/UCe3yC9BC477zw_Q0KLIX6sg = ")
     
     listOfBrowser = ['start chrome '+url,'start firefox '+url]
 
